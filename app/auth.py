cfec979998fe8586287c40eb33c7190dae029f92
Sentineldev <84209942+Sentineldev@users.noreply.github.com>
2021-05-14 19:58:55 -0400

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -2,24 +2,25 @@ from flask import *
 import mysql.connector
 from db import get_db
 from werkzeug.security import generate_password_hash,check_password_hash
-
+#se importa las herramientas de la libreria de flask para renderizar las plantillas
+#se importa la funcion de hash de la libreria de werkzeug para ser guardadas en la base de datos
 
 bp = Blueprint('auth',__name__)
 
 
-
+#se crea la ruta de login
 @bp.route('/login',methods=['POST','GET'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        username = request.form['username']    #Se toman los datos colocados por el usuario
         password = request.form['password']
         db,c = get_db()
         c.execute(
-            'select * from usuario where username = %s',(username,)
+            'select * from usuario where username = %s',(username,)  #se busca en la base de datos un usuario que tenga el username indicado
         )
-        usuario = c.fetchone()
-        if usuario:
-            if check_password_hash(usuario['password'],password):
+        usuario = c.fetchone() #se carga a una variable
+        if usuario: #se valida si la variable contiene datos o no
+            if check_password_hash(usuario['password'],password): #se valida que la contraseña indicada por el usuario sea la misma que esta en la base de datos
                 session.clear()
                 session['user_id'] = usuario['id']
                 return redirect(url_for('user.profile'))
@@ -30,7 +31,7 @@ def login():
 @bp.route('/register',methods=['POST','GET'])
 def register():
     if request.method == 'POST':
-        nombre = request.form['nombre']
+        nombre = request.form['nombre']   #Se toman todos los datos ingresados por el usuario
         apellido = request.form['apellido']
         correo = request.form['correo']
         username = request.form['username']
@@ -38,10 +39,10 @@ def register():
         edad = request.form['edad']
         db,c = get_db()
         c.execute(
-            'select * from usuario where username = %s',(username,)
+            'select * from usuario where username = %s',(username,) #se busca en la base de datos un usuario  con el mismo nombre indicado por el usuario
         )
         usuario = c.fetchone()
-        if not usuario:
+        if not usuario: #se valida si hubo coincidencia o no
             c.execute(
                 'insert into usuario (nombre,apellido,correo,username,password,edad) values (%s,%s,%s,%s,%s,%s)',
                 (nombre,apellido,correo,username,generate_password_hash(password),edad)
