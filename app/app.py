ff0470472b46606df4bea6b102574ddb15287cca
Sentineldev <84209942+Sentineldev@users.noreply.github.com>
2021-05-14 19:54:47 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 from flask import redirect,Flask,url_for
-
+#se importan las dependecias de flask.
 def create_app():
-    app = Flask(__name__)
+    #Se crea el objeto de flask seguido se configura con los datos de la base de datos.
+    app = Flask(__name__) 
     app.config.from_mapping(
         SECRET_KEY='nosequeponeraquiXD',
         DATABASE_HOST='localhost',
@@ -9,16 +10,16 @@ def create_app():
         DATABASE_PASSWORD='1234',
         DATABASE ='todo_list'
     )
-
+   
     import db
     db.init_app(app)
-
-
+   
+    #Se importan los modulos para registrar los blueprint de cada a la aplicacion 
     import auth
     import user
     app.register_blueprint(user.bp)
     app.register_blueprint(auth.bp)
-
+    #se añade la ruta principal que redirecciona automaticamente a la pantalla de login
     @app.route('/')
     def index():
         return redirect(url_for('auth.login'))
