f5e2a9a57b57d132d085b45d29b238947565eb33
Sentineldev <84209942+Sentineldev@users.noreply.github.com>
2021-05-14 20:00:05 -0400

    Update user.py

--- app/user.py
+++ app/user.py
@@ -5,7 +5,7 @@ bp = Blueprint('user',__name__)
 
 @bp.route('/profile')
 def profile():
-    if session.get('user_id'):
+    if session.get('user_id'): #si el usuario ha iniciado sesion se muestra la plantilla del perfil
         db,c = get_db()
         c.execute(
             'select nombre,apellido,correo,edad,username from usuario where id = %s',(session.get('user_id'),)
@@ -19,6 +19,6 @@ def profile():
 @bp.route('/todo-list')
 def todos():
     if session.get('user_id'):
-        return render_template('user/todo.html')
+        return render_template('user/todo.html') #si el usuario iniicio sesion se muestra la plantilla de los todos.
     else:
         return redirect(url_for('auth.login'))
